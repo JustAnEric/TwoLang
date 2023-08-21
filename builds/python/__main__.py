6fbcb91b1726d1e4a90e4aee0e77b8ac254b17f3
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-08-21 20:07:58 +1000

    Create __main__.py

---
+++ builds/python/__main__.py
@@ -0,0 +1,8 @@
+## TwoLang Python interpreter that switches your language into easy mode.
+
+import sys
+import os
+from twolang_ import Interpreter, ErrorCode, Error
+
+print("TwoLang interpreter -- v2023.08")
+
